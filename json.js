38ab8b44cfe2b38f7eb31d8413a1cb864f4049e4
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-10-10 12:02:30 +0600

    fetching url for api

--- json.js
+++ json.js
@@ -6,6 +6,11 @@ const StudentJSON = JSON.stringify(student)
 // Conver json to js object
 const studentObj = JSON.parse(StudentJSON)
 
+const numbers = [10,20,30,40,50,60,70]
+
+const numbersJson = JSON.stringify(numbers)
+
 console.log(student)
 console.log(StudentJSON)
 console.log(studentObj)
+console.log(numbersJson)
